978b2a31ce9602b5ae1bbb80e2d02636b738d5bf
azag666 <157975443+azag666@users.noreply.github.com>
2026-03-23 22:52:15 -0300

    Update API token in utmify.js

--- netlify /functions/utmify.js
+++ netlify /functions/utmify.js
@@ -1,5 +1,4 @@
 exports.handler = async function(event, context) {
-  // Apenas aceita requisições do tipo POST
   if (event.httpMethod !== 'POST') {
     return { statusCode: 405, body: 'Metodo nao permitido' };
   }
@@ -7,12 +6,11 @@ exports.handler = async function(event, context) {
   try {
     const payload = JSON.parse(event.body);
 
-    // O servidor do Netlify faz o envio para a Utmify (Livre de bloqueios de CORS do navegador)
     const response = await fetch('https://api.utmify.com.br/api-credentials/orders', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'x-api-token': 'ADBAMVq173xgWxDoYzBkl2JxXs4b34gpXqQ0' // Seu token protegido no servidor!
+        'x-api-token': 'ZtYj7fZ1Zj1QsCaWh4BphViDxNgRHrTa2Ohm' // <- NOVO TOKEN ATUALIZADO AQUI
       },
       body: JSON.stringify(payload)
     });
